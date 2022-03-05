d7e9366f7ab457fc5d0fb631f4f22ff946acbc09
unknown <luispomare92@gmail.com>
2022-03-05 13:01:38 -0500

    Create "read dom elements" module

---
+++ modules/domReading.js
@@ -0,0 +1,16 @@
+// Dom elements object
+export default class DomObject {
+  // Read elements from document
+  bookInput = document.getElementById("bookInput");
+  authorInput = document.getElementById("authorInput");
+  addButton = document.getElementById("addButton");
+  booksContainer = document.getElementById("booksContainer");
+  div = document.getElementById("div");
+  date = document.getElementById("date");
+  addContainer = document.getElementById("form");
+  contactContainer = document.getElementById("contactContainer");
+  firstItem = document.getElementById("firstItem");
+  secondItem = document.getElementById("secondItem");
+  thirdItem = document.getElementById("thirdItem");
+  mainContainer = document.getElementById("mainContainer");
+}

df83a94bc9288c3bdfa23e5561ec88875d038ce8
unknown <luispomare92@gmail.com>
2022-03-05 20:28:25 -0500

    Create "switchPages" module to change page content dynamicly

--- index.js
+++ index.js
@@ -89,34 +89,4 @@ setInterval(() => {
   domObject.date.innerHTML = Date();
 }, 1000);
 
-domObject.firstItem.addEventListener("click", () => {
-  domObject.mainContainer.classList.add("books");
-  domObject.mainContainer.classList.remove("noVisible");
-  domObject.addContainer.classList.add("noVisible");
-  domObject.contactContainer.classList.add("noVisible");
-  domObject.firstItem.classList.add("selected");
-  domObject.secondItem.classList.remove("selected");
-  domObject.thirdItem.classList.remove("selected");
-});
-
-domObject.secondItem.addEventListener("click", () => {
-  domObject.addContainer.classList.add("flexColumn");
-  domObject.addContainer.classList.remove("noVisible");
-  domObject.mainContainer.classList.add("noVisible");
-  domObject.contactContainer.classList.add("noVisible");
-  domObject.firstItem.classList.remove("selected");
-  domObject.secondItem.classList.add("selected");
-  domObject.thirdItem.classList.remove("selected");
-});
-
-domObject.thirdItem.addEventListener("click", () => {
-  domObject.contactContainer.classList.add("flexColumn");
-  domObject.contactContainer.classList.remove("noVisible");
-  domObject.addContainer.classList.add("noVisible");
-  domObject.mainContainer.classList.add("noVisible");
-  domObject.firstItem.classList.remove("selected");
-  domObject.secondItem.classList.remove("selected");
-  domObject.thirdItem.classList.add("selected");
-});
-
-console.log(createListeners);
+createListeners();
